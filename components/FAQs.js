da3bcc8997658d2098182753a764836ceaf5015a
JojoDuke <jojoamankwa@gmail.com>
2022-05-19 00:15:52 +0000

    scary

--- components/FAQs.js
+++ components/FAQs.js
@@ -2,18 +2,23 @@ import styles from '../styles/FAQs.module.css'
 import {useRef, useEffect, useState} from 'react';
 
 const FAQs = () => {
-  const [isToggled, setIsToggled] = useState(false);
+  const [isToggled1, setIsToggled1] = useState(false);
+  const [isToggled2, setIsToggled2] = useState(false);
 
-  const toggler = () => {
-    isToggled ? setIsToggled(false) : setIsToggled(true);
+  const toggler1 = () => {
+    isToggled1 ? setIsToggled1(false) : setIsToggled1(true);
+  }
+
+  const toggler2 = () => {
+    isToggled2 ? setIsToggled2(false) : setIsToggled2(true);
   }
 
   return (
     <div class={styles.container}>
       <div class={styles.accordion}>
         <div class={styles.accordionItem}>
-          <button id="accordion-button-1" aria-expanded={isToggled} onClick={toggler}>
-            <span class={styles.accordionTitle}>Why is the moon sometimes out during the day?</span>
+          <button id="accordion-button-1" aria-expanded={isToggled1} onClick={toggler1}>
+            <span class={styles.accordionTitle}>Question 1</span>
             <span class={styles.icon} aria-hidden="true"></span>
           </button>
           <div class={styles.accordionContent}>
@@ -26,8 +31,8 @@ const FAQs = () => {
         </div>
 
         <div class={styles.accordionItem}>
-          <button id="accordion-button-1" aria-expanded={isToggled} onClick={toggler}>
-            <span class={styles.accordionTitle}>Why is the moon sometimes out during the day?</span>
+          <button id="accordion-button-1" aria-expanded={isToggled2} onClick={toggler2}>
+            <span class={styles.accordionTitle}>Question 2</span>
             <span class={styles.icon} aria-hidden="true"></span>
           </button>
           <div class={styles.accordionContent}>
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import Layout from '../components/Layout'
 import '../styles/globals.css'
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>Midas - Virtual Credit Cards Powered By MoMo</title>
+        <meta name="description" content="Midas description for web" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   )
