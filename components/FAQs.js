4425d03b7bb27f49bb24d4471a20bb97671f77c6
JojoDuke <jojoamankwa@gmail.com>
2022-05-21 16:45:47 +0000

    wizzy

--- components/FAQs.js
+++ components/FAQs.js
@@ -14,14 +14,14 @@ const FAQs = () => {
   }
 
   return (
-    <div class={styles.container}>
-      <div class={styles.accordion}>
-        <div class={styles.accordionItem}>
+    <div className={styles.container}>
+      <div className={styles.accordion}>
+        <div className={styles.accordionItem}>
           <button id="accordion-button-1" aria-expanded={isToggled1} onClick={toggler1}>
-            <span class={styles.accordionTitle}>Question 1</span>
-            <span class={styles.icon} aria-hidden="true"></span>
+            <span className={styles.accordionTitle}>Question 1</span>
+            <span className={styles.icon} aria-hidden="true"></span>
           </button>
-          <div class={styles.accordionContent}>
+          <div className={styles.accordionContent}>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
               incididunt ut labore et dolore magna aliqua. Elementu sagittis vitae et leo duis ut.
@@ -30,12 +30,12 @@ const FAQs = () => {
           </div>
         </div>
 
-        <div class={styles.accordionItem}>
+        <div className={styles.accordionItem}>
           <button id="accordion-button-1" aria-expanded={isToggled2} onClick={toggler2}>
-            <span class={styles.accordionTitle}>Question 2</span>
-            <span class={styles.icon} aria-hidden="true"></span>
+            <span className={styles.accordionTitle}>Question 2</span>
+            <span className={styles.icon} aria-hidden="true"></span>
           </button>
-          <div class={styles.accordionContent}>
+          <div className={styles.accordionContent}>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
               incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut.
